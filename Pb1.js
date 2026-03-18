a83f792d0787d237fe9058e0cb637f534a796d13
Subrat Kumar Rout <subratrout986@gmail.com>
2026-03-18 22:56:56 +0530

    first commit

---
+++ Pb1.js
@@ -0,0 +1,62 @@
+// let cou = prompt("Enter a number");
+
+// for(let i = 1;i <= cou;i++){
+//     console.log(i);
+// }
+
+// let cou = 10;
+
+// for(let i =1;i<=cou;i++){
+//     console.log(cou-i+1);
+// }
+
+
+// let n = 10;
+
+// for(let i =2;i <= n/2;i++){
+//     if(i % 2 == 0){
+//         console.log(i);
+//     }
+// }
+
+// let n = 4;
+// let fac = 1;
+
+// for(let i = 1;i<=n ;i++){
+//     if(n == (0||1)) fac = 1;
+//     if(n >= 2){
+//         fac *= i;
+//     }
+// }
+// console.log(fac);
+
+// let n = 30;
+
+// for(let i = 1;i <= n;i++){
+//     if(i % 3==0&& i % 5==0){
+//         console.log(i);
+//     }
+// }
+
+// let n = 1;
+// let m = 20;
+
+// for(let i = n ; i <= Math.sqrt(m) ; i++){
+//     let sq = i * i;
+//     if(sq < m){
+//         if(i % 2 === 0 && sq % 2 === 0){
+//             console.log(i);
+//             console.log(sq);
+//         }
+//     }
+// }
+
+// let i =2;
+// const isEven = i % 2 === 0;
+// console.log(typeof(isEven))
+
+// let i = '3'
+// if(i == 3){
+//     console.log(i);
+// }
+
---
+++ Pb2.js
@@ -0,0 +1,30 @@
+// let a = [3,9,74];
+
+// let max = a[0];
+// for(let i = 0;i< a.length;i++ ){
+//     if(a[i]>max){
+//         max = a[i]
+//     }
+// }
+// console.log(max);
+
+
+// let a = 55;
+// let b = 35;
+// let c = 25;
+
+// let max = 0;
+
+// // for(let i = 0;i < 3;i++){
+//     if(a>b && a>c){
+//         max = a;
+//     }
+//     if(b>a && b>c){
+//         max = b;
+//     }
+//     if(c>a && c>b){
+//         max = c;
+//     }
+// // }
+
+// console.log(max)
